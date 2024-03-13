d3b6c241ce41b8e6cd349424b6f7810595a95a0e
dondonbdon <67012587+dondonbdon@users.noreply.github.com>
2024-03-13 11:37:46 +1100

    Update reverse_negative.c

--- test4/reverse_negative.c
+++ test4/reverse_negative.c
@@ -1,1 +1,26 @@
-/web/cs1521/24T1/activities/reverse_negative/files.ln/reverse_negative.c
+// Read numbers into an array until a negative number is entered
+// then print the numbers in reverse order
+
+#include <stdio.h>
+
+#define ARRAY_LEN 1000
+int numbers[ARRAY_LEN];
+
+int main(void) {
+    int i = 0;
+    while (i < ARRAY_LEN) {
+        int x;
+        scanf("%d", &x);
+        if (x < 0) {
+            break;
+        } else {
+            numbers[i] = x;
+        }
+        i++;
+    }
+
+    while (i > 0) {
+        i--;
+        printf("%d\n", numbers[i]);
+    }
+}
