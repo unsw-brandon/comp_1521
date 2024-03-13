b2a0954a453a99222821ea93dc3092251263848e
dondonbdon <67012587+dondonbdon@users.noreply.github.com>
2024-03-13 11:38:04 +1100

    Update different10.c

--- test4/different10.c
+++ test4/different10.c
@@ -1,1 +1,31 @@
-/web/cs1521/24T1/activities/different10/files.ln/different10.c
+#include <stdio.h>
+
+#define ARRAY_LEN 10
+
+int numbers[ARRAY_LEN];
+
+int main(void) {
+    int x, i, n_seen;
+
+    n_seen = 0;
+    while (n_seen < ARRAY_LEN) {
+        printf("Enter a number: ");
+        scanf("%d", &x);
+
+        i = 0;
+        while (i < n_seen) {
+            if (x == numbers[i]) {
+                break;
+            }
+            i++;
+        }
+
+        if (i == n_seen) {
+            numbers[n_seen] = x;
+            n_seen++;
+        }
+    }
+    printf("10th different number was %d\n", x);
+
+    return 0;
+}
